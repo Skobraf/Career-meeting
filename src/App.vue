29ae161f6c8acb699a0d9a9e2b0782666ce7b4c1
skobraf <skobraf@gmail.com>
2019-04-23 13:57:43 +0100

    added detailTitle Component

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/">Home</router-link>
+      <router-link to="/secteur">Secteur</router-link>
+      <router-link to="/secteur/details">Details</router-link>
     </div>
     <router-view />
   </div>
