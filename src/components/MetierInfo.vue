bbb2f2b4ae9da50a9111ce5805e5f8544b384604
skobraf <skobraf@gmail.com>
2019-04-23 13:58:26 +0100

    metierInfo

---
+++ src/components/MetierInfo.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <div v-if="metiert.nameMetier == indice">
+            <h3>{{metiert.nameMetier}}</h3>
+            <p>{{metiert.diplomeReq}}</p><br>
+            <p>{{metiert.expérience}}</p><br>
+            <p>{{metiert.description}}</p>
+        </div>
+        <h2 v-else>{{metiert.nameMetier}}</h2>
+    <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['metiert', 'indice'],
+    name: 'metierDetail'
+
+}
+</script>
+
+<style>
+.active {
+        color:red;
+    }
+</style>
+
