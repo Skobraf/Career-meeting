2d4e8892c864ab839c4b80be83dbabb057c0ff3c
skobraf <skobraf@gmail.com>
2019-04-23 15:59:53 +0100

    check button is disabled on filier checking

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,12 +1,19 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
+describe("Display metier details", () => {
+  beforeEach(() => {
     cy.visit("/");
     cy.contains('Secteur').click()
     cy.url().should('include', '/secteur')
     cy.contains('Tourisme').click()
     cy.url().should('include', '/secteur/0')
-    cy.get('button').should('disabled', 'true')
   });
+
+  it.only('submit button is disabled', () => {
+    cy.get('button').should('disabled', 'true')
+  })
+  it.only('select filiere', () => {
+    cy.get('input[value="Cuisine"]').check()
+    cy.get('button').should('disabled', 'true')
+  })
 });
