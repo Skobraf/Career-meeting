05d7efcb9f0555f8050e684a645f0d7aff905e34
skobraf <skobraf@gmail.com>
2019-04-23 14:02:30 +0100

    updated VueX store

--- src/store.js
+++ src/store.js
@@ -1,10 +1,26 @@
-import Vue from "vue";
-import Vuex from "vuex";
+import Vue from 'vue'
+import Vuex from 'vuex'
+import getDiplome from './data/getDiplome';
+import listSecteur from './data/listSecteur';
+import listMetier from './data/listMetier';
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {}
-});
+  state: {
+    diplome: getDiplome.diplome,
+    secteur: listSecteur.secteur,
+    metier: listMetier.filier,
+    items: {}
+  },
+  mutations: {
+    MUTATE_ITEMS: (state, items) => {
+      state.items = items;
+    }
+  },
+  actions: {
+    handleClick: (context, items) => {
+      context.commit("MUTATE_ITEMS", items);
+    }
+  }
+})
