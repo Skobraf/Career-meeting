c5decbb48d95077cd5404d9716e71606a6da2b66
skobraf <skobraf@gmail.com>
2019-04-23 14:01:31 +0100

    added Secteur view

---
+++ src/views/Secteur.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="secteur">
+        <ul>
+            <li v-for="secteur, index in secteurs"><router-link :to="{ name: 'secteur', params: { secteurId: index }}"> {{secteur.nameSecteur}}</router-link></li>
+        </ul>
+        <router-view/>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'secteur',
+    computed: {
+        secteurs() {
+            return this.$store.state.secteur;
+        }
+    }
+}
+</script>
