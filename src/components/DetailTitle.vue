03103d2702103a0e83ee70af31ed6bff548f787d
skobraf <skobraf@gmail.com>
2019-04-24 17:17:14 +0100

    added style to the title component

--- src/components/DetailTitle.vue
+++ src/components/DetailTitle.vue
@@ -1,5 +1,10 @@
 <template>
-    <h1>{{title}}</h1>
+    <div class="secteur-title">
+        <div class="logo">
+            <i class="fas fa-hotel"></i>
+        </div>
+        <h2>{{title}}</h2>
+    </div>
 </template>
 <script>
 export default {
@@ -7,3 +12,23 @@ export default {
     name:'detaildTitle'
 }
 </script>
+<style lang="scss" scoped>
+    .secteur-title {
+        display: flex;
+        border-radius: 12px;
+        width: 360px;
+        height: 57px;
+        margin: 0 auto;
+        background: #2E263E;
+        h2 {
+            text-transform: uppercase;
+        }
+    }
+    .logo {
+        padding: 14px;
+        border-radius: 50%;
+        background: #743088;
+        margin: 6px 10px;
+    }
+</style>
+
