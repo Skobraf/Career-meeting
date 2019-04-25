1867ea35fa6b3fb8d3b5ec64f6ca581b34d6d4cb
skobraf <skobraf@gmail.com>
2019-04-25 16:59:51 +0100

    center element in div

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,5 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <!-- <router-link to="/">Home</router-link>
-      <router-link to="/secteur">Secteur</router-link>
-      <router-link to="/slider">Slider</router-link> -->
-    </div>
     <router-view />
   </div>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,11 @@
 <template>
-  <div class="home">
-    <h2>CAREER CENTER</h2>
-    <button @click="handleNavigation">Start</button>
+  <div class="home" v-bind:style="{ backgroundImage: 'url(./images/fond-mosaique-01.png)' }">
+    <div class="home-details">
+        <img src="/images/logo-cc-vector.png" alt="">
+        <p>A la decouverte</p>
+        <p>des parcours des carrier</p>
+      <button @click="handleNavigation">Start</button>
+    </div>
   </div>
 </template>
 
@@ -21,3 +25,18 @@ export default {
   }
 };
 </script>
+<style lang="scss" scoped>
+  .home {
+    background-size: cover;
+    height: 100%;
+    text-align: center;
+    img {
+      width: 200px;
+      }
+    display: flex;           /* establish flex container */
+    flex-direction: column;  /* make main axis vertical */
+    justify-content: center; /* center items vertically, in this case */
+    align-items: center;     /* center items horizontally, in this case */
+  }
+</style>
+
